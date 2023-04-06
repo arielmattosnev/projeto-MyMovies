a37c877cf2dcd8b2796ba661d94cc31170ba0748
Arielmtts7 <arielmattosnev@gmail.com>
2023-04-06 17:01:57 -0300

    feat: main page and description working

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <h3>MyMovies</h3>
       <Outlet />
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,38 @@
+import { useState, useEffect } from "react";
+import MovieCard from "../components/MovieCard";
+
+import "./MoviesGrid.css"
+
+const moviesURL = import.meta.env.VITE_API;
+const moviesAPIKEY = import.meta.env.VITE_API_KEY;
+
 const Home = () => {
-	return (
-		<div>Home</div>
-	);
+  const [topMovies, setTopMovies] = useState([]);
+
+  const getTopRatedMovies = async (url) => {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    setTopMovies(data.results);
+  };
+
+  useEffect(() => {
+    const topRatedUrl = `${moviesURL}top_rated?${moviesAPIKEY}`;
+
+    getTopRatedMovies(topRatedUrl);
+  }, []);
+
+  return (
+    <div className="container">
+      <h2 className="title">Melhores Filmes:</h2>
+      <div className="movies-container">
+        {topMovies.length === 0 && <p>Carregando...</p>}
+
+        {topMovies.length > 0 &&
+          topMovies.map((movie) => <MovieCard movie={movie} key={movie.id} />)}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
